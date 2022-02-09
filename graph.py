97453680530075272e0b7f26709a2bb538f8a6a4
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-02-09 14:39:41 -0500

    Even even even even more math

--- graph.py
+++ graph.py
@@ -3,8 +3,10 @@ from multiprocessing import Pool
 from datetime import datetime
 import math
 import collections
-from typing import Iterator
+from typing import Any, Iterator
 
+import matplotlib.pyplot as plt
+import matplotlib
 import numpy as np
 
 
@@ -12,6 +14,8 @@ Parameter = collections.namedtuple('Parameter', ['start', 'stop', 'inc'])
 
 
 class Vector2D:
+    __slots__ = ('x', 'y')
+
     def __init__(self, initial_x: float = 0, initial_y: float = 0) -> None:
         self.x = initial_x
         self.y = initial_y
@@ -119,51 +123,44 @@ class Vector2D:
 
 
 class Range:
+    __slots__ = ('min', 'max')
+
     def __init__(self, min: float, max: float) -> None:
         self.min = min
         self.max = max
     
-    def __lt__(self, other: float) -> bool:
-        return other < self.min
-    
-    def __le__(self, other: float) -> bool:
-        return other <= self.min
-    
-    def __eq__(self, other: float) -> bool:
-        return self.min <= other <= self.max
-    
-    def __ne__(self, other: float) -> bool:
-        return not self == other
-    
-    def __ge__(self, other: float) -> bool:
-        return other >= self.max
-    
-    def __gt__(self, other: float) -> bool: 
-        return other > self.max
+    def within(self, value: float) -> bool:
+        return self.min <= value <= self.max
 
 
+AIR_DENSITY = 1.225 # kg m^-3
+DRAG_COEFFICIENT = 0.47
+BALL_RADIUS = 0.1143 # m
+CROSSSECTIONAL_AREA = math.pi*(BALL_RADIUS)**2 # m^2
+BALL_MASS = 0.270 # kg
 
-dt = 0.001
-kd = 0.5*1.225*0.47*math.pi*(0.1143)**2/0.270
-kg = 9.81
+TARGET_HEIGHT = 2.64 # m
+TARGET_HEIGHT_TOLERANCE = BALL_RADIUS # m
+TARGET_RADIUS = 0.675 # m
+
+MAX_HEIGHT = 10
 
 ROBOT_HEIGHT = 0.40
 
-magvi = 8
-initial_angle = 60
-
-MAX_HEIGHT = 10
+dt = 0.001
+kd = 0.5*AIR_DENSITY*DRAG_COEFFICIENT*CROSSSECTIONAL_AREA/BALL_MASS # m^-1
+kg = 9.81
 
 def made_goal(
         velocity_magnitude: float,
         angle: float,
         target_distance_center: float,
-        target_height: float = 2.64,
-        target_radius: float = 0.675,
-        target_height_tolerance: float = 0.1143
-        ) -> tuple[bool, bool | None, float, tuple[Vector2D, Vector2D]]:
+        target_height_min: float = TARGET_HEIGHT,
+        target_radius: float = TARGET_RADIUS,
+        target_height_tolerance: float = TARGET_HEIGHT_TOLERANCE,
+        ) -> tuple[bool, bool | None, tuple[Any, ...]]:
     target_distance = Range(target_distance_center - target_radius, target_distance_center + target_radius)
-    target_height = Range(target_height, target_height + target_height_tolerance)
+    target_height = Range(target_height_min, target_height_min + target_height_tolerance)
     vel = Vector2D()
     vel.magnitude = velocity_magnitude
     vel.angle_degrees = angle
@@ -171,7 +168,7 @@ def made_goal(
     t = 0
     y_max = 0
 
-    while t < 100: # not ((v[1] < 0 and p[1] < target_height + target_height_tolerance) or (p[1] >= 10 and p[0] > target_height + target_height_tolerance)):
+    while t < 100:
         acc = vel.copy()
         acc *= -kd * acc.magnitude
         acc.y -= kg
@@ -179,22 +176,19 @@ def made_goal(
         pos += vel * dt
         t += dt
         if pos.y > MAX_HEIGHT: # Ball exceeds height limit
-            return False, False, pos.y, (vel, pos)
+            return False, True, (pos.y, (vel, pos), 0)
         if vel.y < 0: # Ball is falling
-            if pos.y < target_height \
-                    or pos.x * 2 < target_distance: # Ball is below target height or is not far enough
-                return False, False, y_max, (vel, pos)
-            elif pos.x > target_distance: # Ball is past target distance
-                return False, True, y_max, (vel, pos)
+            if pos.y < target_height.min \
+                    or pos.x * 2 < target_distance.min: # Ball is below target height or is not far enough
+                return False, False, (y_max, (vel, pos), 1)
+            elif pos.x > target_distance.max: # Ball is past target distance
+                return False, True, (y_max, (vel, pos), 2)
             
-            elif pos.x == target_distance and pos.y == target_height: # Ball is in goal
-                return True, None, max(y_max), (vel, pos)
+            elif target_distance.within(pos.x) and target_height.within(pos.y): # Ball is in goal
+                return True, None, (y_max, (vel, pos), 3)
         
-        else:
+        else: # Ball is moving upwards
             y_max = pos.y
-            if pos.y < target_height \
-                    and pos.x > target_distance.min: # Ball is below target height and is past goal
-                return False, False, y_max, (vel, pos)
     
     raise Exception('t exceeded limit of 100 seconds')
 
@@ -208,7 +202,7 @@ def iter(start, stop, inc):
 d = Parameter(3, 10, 0.05)
 theta = Parameter(30, 90, 0.1)
 dv = 0.1
-# f = open("C:\\Users\\amumm\\Downloads\\data.txt", mode='w', encoding='UTF-8')
+# f = open("data.txt", mode='w', encoding='UTF-8')
 # for distance in iter(*d):
 #     made_velocities = []
 #     made_angles = []
@@ -223,13 +217,6 @@ dv = 0.1
 #                 made_angles.append(round(angle, 3))
 #     f.write(f'{round(distance, 3)}\t{made_velocities}\t{made_angles}\n')
 
-# Plot phistory on a graph
-# plt.axis([0, max_val, 0, max_val])
-# plt.scatter(made_velocities, made_angles, color="red")
-# plt.xlabel("Velocity (m/s)")
-# plt.ylabel("Angle (degrees)")
-# plt.title("Ball trajectory")
-# plt.show()
 
 def list_iter(list_: list) -> Iterator:
     for i in list_[:-1]:
@@ -249,20 +236,49 @@ def multi_iter(*args: Iterator[float]) -> Iterator[tuple[float, ...]]:
 
 
 def single_run(d, theta):
+    print(f'[{datetime.now()}]: testing distance={round(d, 3)}, angle={round(theta, 3)}')
     made_triplets = []
-    # print(f'[{datetime.now()}]: testing distance={round(d, 3)}, angle={round(theta, 3)}')
+    if math.atan2(TARGET_HEIGHT, d - TARGET_RADIUS) > theta:
+        return made_triplets
     vi = dv
     
     while not (result := made_goal(vi, theta, d))[1]:
-        print(vi)
+        # print(vi, result)
         vi += dv
         if result[0]:
             made_triplets.append((round(d, 3), round(theta, 3), round(vi, 3)))
     
     return made_triplets
 
+def angle_run(d):
+    made_triplets = []
+    for angle in iter(*theta):
+        print(f'[{datetime.now()}]: testing distance={round(d, 3)}, angle={round(theta, 3)}')
+        made_triplets += single_run(d, angle)
+    return made_triplets
+
 if __name__ == '__main__':
-    print(made_goal(9.5, 49.6, 3))
-    # with Pool(math.floor((d.stop - d.start) / d.inc)) as p:
-    #     pass
-    print(single_run(d.start, 49.6))
+    # print(made_goal(9.5, 49.6, 3))
+
+    with open("data3.txt", mode='a', encoding='UTF-8') as f:
+        with Pool() as p:
+            for triplet_set in p.starmap(single_run, multi_iter(iter(*d), iter(*theta))):
+                f.write(f'{triplet_set}\n')
+
+    # la = []
+    # lv = []
+    # for angle in iter(*theta):
+    #     for d, t, v in single_run(4, angle):
+    #         la.append(t)
+    #         lv.append(v)
+    
+    # # Plot history on a graph
+    # with open("data2.txt", mode='r', encoding='UTF-8') as f:
+    #     data = f.readline()
+    #     velocities, angles = eval(data.split('\t')[1]), eval(data.split('\t')[2])
+    # plt.scatter(velocities, angles, color="red")
+    # plt.xlabel("Angle (degrees)")
+    # plt.ylabel("Velocity (m/s)")
+    # plt.title("Successfull shots for distance = 4")
+    # plt.savefig("d=4.png")
+    # plt.show()
