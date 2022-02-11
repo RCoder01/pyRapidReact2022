45236949d941f496b878f33ade505970868da3a7
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-02-11 09:15:11 -0500

    Revert "Change teleop drive commands to RunCommands"
    This reverts commit 106fd8d28e501b87e4763fa2d4df0564f25a590d.

--- src/commands/drivetrain/_arcade_drive.py
+++ src/commands/drivetrain/_arcade_drive.py
@@ -4,14 +4,23 @@ import wpilib.drive
 import subsystems
 
 
-class ArcadeDrive(commands2.RunCommand):
+class ArcadeDrive(commands2.CommandBase):
     def __init__(self, forward_supplier, turning_supplier) -> None:
-        set_speeds = lambda speeds: subsystems.drivetrain.set_speed(speeds.left, speeds.right)
-        run_function = lambda: set_speeds(wpilib.drive.DifferentialDrive.arcadeDriveIK(forward_supplier(), turning_supplier(), False))
-
-        commands2.RunCommand.__init__(self, run_function)
+        commands2.CommandBase.__init__(self)
         self.addRequirements(subsystems.drivetrain)
         self.setName('Arcade Drive')
+
+        self._forward_supplier = forward_supplier
+        self._turning_supplier = turning_supplier
+
+    def execute(self) -> None:
+        speeds = wpilib.drive.DifferentialDrive.arcadeDriveIK(self._forward_supplier(), self._turning_supplier(), False)
+
+        subsystems.drivetrain.set_speed(speeds.left, speeds.right)
+        return super().execute()
+
+    def isFinished(self) -> bool:
+        return False
 
     def end(self, interrupted: bool) -> None:
         subsystems.drivetrain.set_speed(0, 0)
