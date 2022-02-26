601b228d5113b65a6fe9f8b05b64bb89b3a05aa7
Arnav Mummineni <45217840+RCoder01@users.noreply.github.com>
2022-02-25 19:47:52 -0500

    tiny

--- robot.py
+++ robot.py
@@ -1,26 +1,11 @@
-import wpilib
-import wpilib.simulation
-import commands2
-import ctre
+(lambda w,d,c:(lambda r,i:(setattr(r,'teleopInit',lambda s: setattr(s,'d',w.drive.DifferentialDrive(c.WPI_TalonFX(0),c.WPI_TalonFX(1)))),setattr(r,'teleopPeriodic',lambda s: s.d.arcadeDrive(i.getLeftX(),i.getRightY())),w.run(r)))(type('r',(w.TimedRobot,),{}),w.XboxController(0)))(*(lambda i:(i('wpilib'),i('wpilib.drive'),i('ctre')))(__import__))
 
-class Robot(commands2.TimedCommandRobot):
-    def teleopInit(self) -> None:
-    #     self.m = ctre.WPI_TalonFX(2)
-    #     self.ms = [ctre.WPI_TalonFX(id) for id in (6, 7, 8)]
-    #     map(lambda motor: motor.setNeutralMode(ctre.NeutralMode.Coast), self.ms)
-        self.sensor = wpilib.DigitalInput(0)
-        self.simsensor = wpilib.simulation.DIOSim(self.sensor)
-        self.timer = wpilib.Timer()
-    def teleopPeriodic(self) -> None:
-        # wpilib.SmartDashboard.putNumber("encoder distance", self.m.getSelectedSensorPosition())
-        wpilib.SmartDashboard.putNumber("sensor output", self.sensor.get())
-        wpilib.SmartDashboard.putData("sensor", self.sensor)
-        if self.timer.getFPGATimestamp() % 2 // 1:
-            self.simsensor.setValue(True)
-        else:
-            self.simsensor.setValue(False)
-        wpilib.SmartDashboard.putNumber('fpgatimestamp', self.timer.getFPGATimestamp() % 2 // 1)
-        return super().teleopPeriodic()
-
-if __name__ == '__main__':
-    wpilib.run(Robot)
+import wpilib as w
+import wpilib.drive as d
+from ctre import WPI_TalonFX as t
+class r(w.TimedRobot):
+ def teleopInit(s):
+  s.d=d.DifferentialDrive(t(0),t(1))
+  s.c=w.XboxController(0)
+ def teleopPeriodic(s):
+  s.d.arcadeDrive(s.c.getLeftX(),s.c.getRightY())
